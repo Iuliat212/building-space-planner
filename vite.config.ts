c03fb7e4d76383ad1de472364a420cc7ba80a858
Iuliat212 <92670742+Iuliat212@users.noreply.github.com>
2025-09-05 13:10:47 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'
+
 
 export default defineConfig({
+  plugins: [react()],
   base: 'https://Iuliat212.github.io/building-space-planner', // Replace '/your-base-path/' with your desired base URL
   // Other configurations
 });
